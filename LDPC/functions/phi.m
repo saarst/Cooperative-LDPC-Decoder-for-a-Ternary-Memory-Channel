4d2c78ee3690ad63497763c52423b58b13e08e51
saarst <47382261+saarst@users.noreply.github.com>
2023-06-03 13:34:04 +0300

    Update phi.m

--- LDPC/functions/phi.m
+++ LDPC/functions/phi.m
@@ -1,8 +1,8 @@
 function y = phi(x)
-    y = -log(tanh(x/2));
-    if y == inf
-        % warning("phi output is inf, changing to 10e9");
-        y= 10e9;
+    if x <= eps
+        y = 40;
+        return
     end
+    y = -log(tanh(x/2));
 end
 
