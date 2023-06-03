d25a18d818f444a3fcb58bd99a51b2ece59e57a5
saarst <47382261+saarst@users.noreply.github.com>
2023-06-03 13:50:19 +0300

    Update PBS_loop.m

--- PBS_loop.m
+++ PBS_loop.m
@@ -1,4 +1,4 @@
 for log_p = -4.5:0.25:-2.5
     name = sprintf("TriLDPC_logp_%g_n_256_R_05",log_p);
-    system(sprintf("qsub -N %s -v log_p=%g ./PBS_main.sh", name, log_p));
+    system(sprintf("qsub -N %s -j oe -o ./logs/%s.log -v log_p=%g ./PBS_main.sh", name, name, log_p));
 end
