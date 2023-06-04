65269fdb92707b179b5a13d49abf958165e36ed3
saarst <47382261+saarst@users.noreply.github.com>
2023-06-04 23:30:07 +0300

    Update RunPBS.m

--- RunPBS.m
+++ RunPBS.m
@@ -1,8 +1,9 @@
-function RunPBS(experimentName, logps, sequence, n, R, numIter, batchSize)
+function RunPBS(experimentName, logps, sequenceInd, sequenceRes, n, R, numIter, batchSize)
     arguments
         experimentName = "TriLDPC"  % Default experiment name
         logps = -5:-3  % Default range of log_p values
-        sequence = [20, 20]  % Default sequence array
+        sequenceInd = 2  % Default sequence array
+        sequenceRes = 2
         n = 256  % Default value for n
         R = 0.5  % Default value for R
         numIter = 10.^(-logps + 2)  % Default numIter values based on logps
@@ -11,7 +12,7 @@ function RunPBS(experimentName, logps, sequence, n, R, numIter, batchSize)
     
     for log_p = logps
         % Format and execute the qsub command with all the parameters
-        system(sprintf("qsub -V -v log_p=%g,experimentName='%s',sequence=[%s],n=%d,R=%g,numIter=%g,batchSize=%d ./PBS_main.sh", log_p, experimentName, strjoin(string(sequence), ','), n, R, numIter(log_p - logps(1) + 1), batchSize));
+        system(sprintf("qsub -v log_p=%g,experimentName='%s',sequenceInd=%d, sequenceRes=%d,n=%d,R=%g,numIter=%g,batchSize=%d ./PBS_main.sh", log_p, experimentName, sequenceInd,sequenceRes, n, R, numIter(log_p - logps(1) + 1), batchSize));
     end
 
 end
