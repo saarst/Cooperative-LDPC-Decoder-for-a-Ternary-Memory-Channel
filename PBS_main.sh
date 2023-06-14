befe4c9518923c494f90811222d780d1400a3225
saarst <47382261+saarst@users.noreply.github.com>
2023-06-14 16:31:29 +0300

    Update PBS_main.sh

--- PBS_main.sh
+++ PBS_main.sh
@@ -5,7 +5,7 @@
 #PBS -l select=2:ncpus=12
 #PBS -l select=mem=20GB
 #PBS -l walltime=24:00:00
-#PBS -e ./logs/${experimentName}/${PBS_JOBNAME}.e${PBS_JOBID}
+#PBS -e $HOME/project_1/TriLDPC/logs/${experimentName}/${PBS_JOBNAME}.e${PBS_JOBID}
 
 PBS_O_WORKDIR=$HOME/project_1/TriLDPC
 cd $PBS_O_WORKDIR
@@ -13,4 +13,4 @@ cd $PBS_O_WORKDIR
 logs_dir="$PBS_O_WORKDIR/logs/${experimentName}"
 
 # Run MATLAB command and redirect output to the output file
-"/usr/local/matlab/bin/matlab" -nodisplay -r "ternary_batch_simulation_main(${n}, ${log_p}, ${R}, ${numIter}, ${batchSize}, ${sequenceInd}, ${sequenceRes}, %{ratio}, './Results/${experimentName}' )" > "$logs_dir/${PBS_JOBNAME}.o${PBS_JOBID}" 
+"/usr/local/matlab/bin/matlab" -nodisplay -r "ternary_batch_simulation_main(${n}, ${log_p}, ${R}, ${numIter}, ${batchSize}, ${sequenceInd}, ${sequenceRes}, %{ratio}, './Results/${experimentName}' )" > "$logs_dir/${PBS_JOBNAME}.o${PBS_JOBID}" 2> > "$logs_dir/${PBS_JOBNAME}.e${PBS_JOBID}" 
