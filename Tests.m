e5763aa9b001c32e8dbb498730bdeadff11f8fe1
saarst <47382261+saarst@users.noreply.github.com>
2023-08-06 20:03:33 +0300

    Update Tests.m

--- Tests.m
+++ Tests.m
@@ -7,7 +7,7 @@ T = load("testWaterfall.mat","T").T;
 date = char(datetime('now','TimeZone','local','Format','ddMMHHmm'));
 
 n = 128;
-numIter = "48e3";
+numIter = "24e3";
 for i=1:height(T)
     ratio = T{i,1};
     sequence = T{i,2}{1};
@@ -18,6 +18,6 @@ for i=1:height(T)
     % d - date, I - num of Iterations, r - ratio, 
     % si - indicator part of sequence, sr - residual part of seuquence
     % RunPBS(experimentName, -3:0.1:0.1, sequence(1), sequence(2), ratio, n, rate(1), rate(2), str2double(numIter))
-    RunPBS(experimentName, -3, sequence(1), sequence(2), ratio, n, rate(1), rate(2), str2double(numIter))
+    RunPBS(experimentName, -3:0.1:0.1, sequence(1), sequence(2), ratio, n, rate(1), rate(2), str2double(numIter))
 
 end
