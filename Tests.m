8fdd060dc11602a4a067821df5e389db0581cf1f
saarst <47382261+saarst@users.noreply.github.com>
2023-08-12 21:41:09 +0300

    testPQ3

--- Tests.m
+++ Tests.m
@@ -1,10 +1,10 @@
 % % Tests:
-% log_p = {log10(0.05)}; % downward errors
+% log_p = {log10(0.001)}; % downward errors
 % log_q = {log10(linspace(0.01,0.2,20))};
 % sequences = {[4 2]};
 % rates = {[0.5, 0.4]}; % first is ind, second is res
 % T = combinations(log_p, log_q, sequences, rates);
-T = load("testPQ2.mat","T").T;
+T = load("testPQ3.mat","T").T;
 date = char(datetime('now','TimeZone','local','Format','ddMMHHmm'));
 
 n = 128;
