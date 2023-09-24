81b6c2e74d7be93c0916c673eb22994f754352f4
saarst <47382261+saarst@users.noreply.github.com>
2023-09-24 11:48:59 +0300

    n=256 test

--- TestsBSC.m
+++ TestsBSC.m
@@ -1,15 +1,7 @@
-% % Tests:
-% log_p = {log10(0.001)}; % downward errors
-% log_q = {log10(linspace(0.01,0.2,20))};
-% sequences = {[4 2]};
-% rates = {[0.75, 0.6]}; % first is ind, second is res
-% T = combinations(log_p, log_q, sequences, rates);
-% T = load("testPQ4.mat","T").T;
-
 date = char(datetime('now','TimeZone','local','Format','ddMMHHmm'));
-n = 648;
-% log_p = log10(linspace(0.005,0.08,20));
-log_p = log10(0.005);
+n = 256;
+log_p = log10(linspace(0.005,0.08,20));
+% log_p = log10(0.005);
 
 numIter = ceil(10.^(-min(log_p)+2));
 rate = 0.5;
