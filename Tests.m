0aba0f2ebfa7ad4aa8b01f0446da51d3669b6d1c
saarst <47382261+saarst@users.noreply.github.com>
2023-10-03 12:16:40 +0300

    Update Tests.m

--- Tests.m
+++ Tests.m
@@ -1,13 +1,18 @@
+sequenceCell = { [2,2], [4,2]};
+ratesCell = {[0.75, 0.75], [0.75, 0.5]};
+
+for ii = 1:length(sequenceCell)
+    for jj = 1:length(ratesCell)
 date = char(datetime('now','TimeZone','local','Format','ddMMHHmm'));
 n = 128;
 p = 0.001;
-q = linspace(0.001,0.02,20);
+q = logspace(log10(0.001),log10(0.4),5);
 log_p = log10(p);
 log_q = log10(q);
 [P,Q] = meshgrid(log_p, log_q);
 numIter = ceil(10.^(-min(P,Q)+2)) + 10e3;
-sequence = [2, 2];
-rates = [0.5, 0.3];
+sequence = sequenceCell{ii};
+rates = ratesCell{jj};
 rateIndStr = string(rates(1)).replace(".","");
 rateResStr = string(rates(2)).replace(".","");
 experimentName = sprintf("TriLDPC_d%s_n%d_si%d_sr%d_Ri%s_Rr%s",date, n, sequence(1), sequence(2), rateIndStr, rateResStr);
@@ -22,3 +27,5 @@ save(ResultsFolder + "/pq.mat","log_p","log_q");
 % d - date, I - num of Iterations, r - ratio, 
 % si - indicator part of sequence, sr - residual part of seuquence
 RunPBS(experimentName, numIter, log_p, log_q, sequence(1), sequence(2), n, rates(1), rates(2))
+    end
+end
