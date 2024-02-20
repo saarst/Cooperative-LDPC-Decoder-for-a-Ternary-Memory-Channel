d4a83cb4a170c9a3d67ccf12b7f62760b787740d
saarst <47382261+saarst@users.noreply.github.com>
2024-02-20 21:13:38 +0200

    bugfix

--- LDPC/Channels/bac_llr.m
+++ LDPC/Channels/bac_llr.m
@@ -3,6 +3,6 @@ function f = bac_llr(p,q)
 % p - probability of down 1 -> 0
 % q - probability of up   0 -> 1
 %   Detailed explanation goes here
-    f = @(y) (y==1) * log(p/(1-p))  +  (y==0) * log((1-q)/q);
+    f = @(y) (y==1) * log(q/(1-p))  +  (y==0) * log((1-q)/p);
 end
 
